83895240ea2232afe0e6fab8d26a61ac6944b1fa
Brainton <hbraiton@gmail.com>
2023-07-10 16:15:32 -0300

    Inicio de Proyecto

---
+++ src/main/java/Alkemy/BilleteraVirtual/BilleteraVirtualApplication.java
@@ -0,0 +1,13 @@
+package Alkemy.BilleteraVirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilleteraVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilleteraVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Alkemy/BilleteraVirtual/BilleteraVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package Alkemy.BilleteraVirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilleteraVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

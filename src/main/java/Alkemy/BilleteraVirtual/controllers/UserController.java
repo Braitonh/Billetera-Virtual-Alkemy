3110149156424ab119b91b29db184f028e3ac5ad
Brainton <hbraiton@gmail.com>
2023-07-11 21:33:47 -0300

    Creacion del modelo User, Endpoint Post y Get

--- src/main/java/Alkemy/BilleteraVirtual/controllers/UserController.java
+++ src/main/java/Alkemy/BilleteraVirtual/controllers/UserController.java
@@ -5,34 +5,28 @@ import Alkemy.BilleteraVirtual.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping(path = "users")
+@RequestMapping(path = "auth")
 public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping(path = "save")
-    public ResponseEntity<?> createUser(@RequestBody User user){
+    @PostMapping(path = "/register")
+    public ResponseEntity<Object> registerUser(@RequestBody User user){
 
-        try {
+        return userService.newUser(user);
 
-           User newUser = userService.saveUser(user);
-
-           return ResponseEntity.ok(newUser);
-
-        }catch (Exception e){
-
-            String message = e.getMessage();
-
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
-
-        }
 
     }
 
+    @GetMapping(path = "/users")
+    public List<User> getAllUsers(){
+
+        return userService.getAllUsers();
+
+    }
 }
--- src/main/java/Alkemy/BilleteraVirtual/services/UserService.java
+++ src/main/java/Alkemy/BilleteraVirtual/services/UserService.java
@@ -1,10 +1,17 @@
 package Alkemy.BilleteraVirtual.services;
 
+
 import Alkemy.BilleteraVirtual.entities.User;
 import Alkemy.BilleteraVirtual.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -12,18 +19,39 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    //private BCryptPasswordEncoder encrypter;
+    public ResponseEntity<Object> newUser(User user){
 
-    public User saveUser (User user){
+        Optional<User> res = userRepository.findByEmail(user.getEmail());
 
-        User newUser = user;
+        HashMap<String, Object> datos = new HashMap<>();
 
-        //newUser.setPassword(encrypter.encode(newUser.getPassword()));
-
-        userRepository.save(newUser);
-
-        return  newUser;
+        if (res.isPresent()){
+            datos.put("Error",true);
+            datos.put("message", "Usuario existente");
+            return new ResponseEntity<>(
+                    datos,
+                    HttpStatus.CONFLICT
+            );
+        }else {
+            userRepository.save(user);
+            datos.put("data",user);
+            datos.put("message", "Usuario registrado correctamente");
+            return new ResponseEntity<>(
+                    datos,
+                    HttpStatus.CREATED
+            );
+        }
 
     }
+
+
+
+    public List<User> getAllUsers(){
+
+        return userRepository.findAll();
+
+    }
+
+
 
 }
